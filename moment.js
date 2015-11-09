91f846ff1f736ecd3dfe43f6584154e4d773beaa
chipolinka <chipolinka96@gmail.com>
2015-11-10 02:22:15 +0500

    Моё решение задачи

--- moment.js
+++ moment.js
@@ -1,20 +1,90 @@
 'use strict';
 
 module.exports = function () {
+    var week = ['ПН', 'ВТ', 'СР', 'ЧТ', 'ПТ', 'СБ', 'ВС'];
     return {
         // Здесь как-то хранится дата ;)
-        date: null,
+        get date () {
+            return parseTime(this.format('%DD %HH:%MM+0'));
+        },
+        set date (minutes) {
+            this.dates = getTimeFromMinutes(minutes);
+        },
+
+        dates: {},
 
         // А здесь часовой пояс
-        timezone: null,
+        timezone: 5,
 
         // Выводит дату в переданном формате
         format: function (pattern) {
+            var days = this.dates.days;
+            var hours = this.dates.hours + this.timezone;
+            if (hours >= 24) {
+                hours -= 24;
+                days++;
+            }
+            if (hours < 0) {
+                hours += 24;
+                days--;
+            }
+            days = days == -1 ? week[week.length - 1] : week[days];
+            pattern = pattern.replace(/%DD/g, days);
+            hours = hours < 10 ? '0' + hours : hours;
+            pattern = pattern.replace(/%HH/g, hours);
+            var minutes = this.dates.minutes;
+            minutes = minutes < 10 ? '0' + minutes : minutes;
+            pattern = pattern.replace(/%MM/g, minutes);
+            return pattern;
         },
 
         // Возвращает кол-во времени между текущей датой и переданной `moment`
         // в человекопонятном виде
         fromMoment: function (moment) {
+            var currentMoment = new Date();
+            var curMin = currentMoment.getMinutes();
+            var curHour = currentMoment.getUTCHours();
+            var curDay = currentMoment.getUTCDay();
+            var diffMin = moment.date - parseTime(curDay + ' ' + curHour + ':' + curMin + '+0');
+            var diff = getTimeFromMinutes(diffMin);
+            var str = 'До ограбления ';
+            if (diff.days != 0) {
+                str = str + 'остался ' + diff.days + ' день ';
+            } else {
+                str = str + 'осталось ';
+            }
+            if (diff.hours != 0) {
+                str = str + diff.hours + ' часов ';
+            }
+            str = str + diff.minutes + ' минут';
+            console.log(str);
+            return str;
         }
     };
 };
+
+function getTimeFromMinutes(minutes) {
+    var day = 24 * 60;
+    var countDays = Math.floor(minutes / day);
+    minutes = minutes % day;
+    var hour = 60;
+    var countHours = Math.floor(minutes / hour);
+    minutes = minutes % hour;
+    return {
+        days: countDays,
+        hours: countHours,
+        minutes: minutes
+    };
+}
+
+function parseTime(strTime) {
+    var days = ['ПН', 'ВТ', 'СР'];
+    var day = days.indexOf(strTime.substr(0, 2));
+    var hours = parseInt(strTime.substr(3, 2));
+    var minutes = parseInt(strTime.substr(6, 2));
+    var zone = strTime.substr(8);
+    var sign = zone.charAt(0) == '-' ? 1 : -1;
+    var timezone = parseInt(zone.substr(1)) * sign;
+    var time = minutes + 60 * (timezone + hours + 24 * day);
+    return time;
+}
--- robbery.js
+++ robbery.js
@@ -5,11 +5,23 @@ var moment = require('./moment');
 // Выбирает подходящий ближайший момент начала ограбления
 module.exports.getAppropriateMoment = function (json, minDuration, workingHours) {
     var appropriateMoment = moment();
-
     // 1. Читаем json
-    // 2. Находим подходящий ближайший момент начала ограбления
-    // 3. И записываем в appropriateMoment
+    var parsed = JSON.parse(json);
+    var data = getMinutes(parsed);
 
+    var bank = [
+        parseTime('ПН ' + workingHours.from), parseTime('ПН ' + workingHours.to),
+        parseTime('ВТ ' + workingHours.from), parseTime('ВТ ' + workingHours.to),
+        parseTime('СР ' + workingHours.from), parseTime('СР ' + workingHours.to)
+    ];
+    var dataFreeTime = getFreeTime(data);
+    dataFreeTime.push(bank);
+
+    // 2. Находим подходящий ближайший момент начала ограбления
+    var intersection = getIntersection(dataFreeTime, minDuration);
+
+    // 3. И записываем в appropriateMoment
+    appropriateMoment.date = intersection[0];
     return appropriateMoment;
 };
 
@@ -22,3 +34,102 @@ module.exports.getStatus = function (moment, robberyMoment) {
 
     return 'Ограбление уже идёт!';
 };
+
+function getMinutes(data) {
+    var result = [];
+    var numOfRobber = 0;
+    for (var robber in data) {
+        result.push([]);
+        for (var time in data[robber]) {
+            result[numOfRobber].push({});
+            result[numOfRobber][time].from = parseTime(data[robber][time].from);
+            result[numOfRobber][time].to = parseTime(data[robber][time].to);
+        }
+        numOfRobber++;
+    }
+    return result;
+}
+
+function parseTime(strTime) {
+    var days = ['ПН', 'ВТ', 'СР'];
+    var day = days.indexOf(strTime.substr(0, 2));
+    var hours = parseInt(strTime.substr(3, 2));
+    var minutes = parseInt(strTime.substr(6, 2));
+    var timezone = getTimeZone(strTime.substr(8));
+    var time = minutes + 60 * (timezone + hours + 24 * day);
+    return time;
+}
+
+function getTimeZone(timezone) {
+    var sign = timezone.charAt(0) == '-' ? 1 : -1;
+    var hours = parseInt(timezone.substr(1));
+    return hours * sign;
+}
+
+function getFreeTime(data) {
+    var segments = [];
+    for (var numOfRobber = 0; numOfRobber < data.length; numOfRobber++) {
+        var points = [];
+        points.push(0);
+        for (var day = 0; day < data[numOfRobber].length; day++) {
+            points.push(data[numOfRobber][day].from);
+            points.push(data[numOfRobber][day].to);
+        }
+        points.push(parseTime('СР 23:59+5'));
+        segments.push(points);
+    }
+    return segments;
+}
+
+function getIntersection(data, minDuration) {
+    var robbers = getIntersectionLast(data, minDuration);
+
+    while (robbers.length > 1) {
+        robbers = getIntersectionLast(robbers, minDuration);
+    }
+    if (robbers[0].length == 0) {
+        return null;
+    } else {
+        return robbers[0];
+    }
+}
+
+function getIntersectionLast(data, minDuration) {
+    var last = data[data.length - 1];
+    var robbers = [];
+
+    for (var numOfRobber = 0; numOfRobber < data.length - 1; numOfRobber++) {
+        robbers.push([]);
+        var robber = data[numOfRobber];
+        for (var hoursRobber = 0; hoursRobber < robber.length; hoursRobber += 2) {
+            var robFrom = robber[hoursRobber];
+            for (var hoursLast = 0; hoursLast < last.length; hoursLast += 2) {
+                if (robber[hoursRobber + 1] <= last[hoursLast]) {
+                    break;
+                }
+                if (robFrom >= last[hoursLast + 1]) {
+                    continue;
+                }
+                robFrom = Math.max(robFrom, last[hoursLast]);
+                var robTo = Math.min(robber[hoursRobber + 1], last[hoursLast + 1]);
+                if (robTo - robFrom < minDuration) {
+                    continue;
+                }
+                robbers[numOfRobber].push(robFrom);
+                robbers[numOfRobber].push(robTo);
+            }
+        }
+    }
+    return robbers;
+}
+
+function getTimeFromMinutes(minutes) {
+    var days = ['ПН', 'ВТ', 'СР'];
+    var day = 24 * 60;
+    var countDays = Math.floor(minutes / day);
+    minutes = minutes % day;
+    var hour = 60;
+    var countHours = Math.floor(minutes / hour);
+    minutes = minutes % hour;
+    return days[countDays] + ' ' + countHours + ':' + minutes + '+0';
+}
